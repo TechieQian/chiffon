ae1efffa2daf28feb93c8cd550a9d5269d9b4025
Qian Wan <qiansan@gmail.com>
2021-01-17 16:17:13 -0500

    editing grooming

--- src/body/Grooming.tsx
+++ src/body/Grooming.tsx
@@ -36,6 +36,15 @@ function Grooming(props : any) {
 </div>
 </div>
 </div>
+<div className="tile is-parent">
+    <article className="tile is-child notification is-info">          
+    <p className="title"> Middle tile</p>
+    <p className="subtitle">With an image</p>
+         <figure className="image is-4by3">
+        <img src="https://bulma.io/images/placeholders/640x480.png">
+        </figure>
+    </article>
+</div>
 
 
             </div>
