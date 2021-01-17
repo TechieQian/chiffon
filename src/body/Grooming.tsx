6622c3326e96c938429d6d1bd73d1a26ea4b230f
Qian Wan <qiansan@gmail.com>
2021-01-17 15:39:01 -0500

    editing grooming

--- src/body/Grooming.tsx
+++ src/body/Grooming.tsx
@@ -6,10 +6,12 @@ function Grooming(props : any) {
             <h1 className="title"> DIY Grooming Tips:</h1>
             <p>Ever been to 5 different dog groomers in NYC asking for a puppy cut and having your dog come out looking different every time? Or do you have a disaster story from the groomers who you would prefer not to return back to?
 </p>
-<p></p>
 <p>You only have two options:</p>
 <p>1. Continue searching for a groomer you like</p>
 <p>2. Take matters in your hands and do it yourself (I choose to groom Chiffon myself). Here are some of my tips below:</p>
+<p class="px-1"> Horizontal padding</p>
+
+
 <p></p>
             </div>
     )
