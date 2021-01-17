21dcf5f63643229bd299a5222ffbbbd610b9bab9
Qian Wan <qiansan@gmail.com>
2021-01-17 15:37:40 -0500

    editing grooming

--- src/body/Grooming.tsx
+++ src/body/Grooming.tsx
@@ -4,15 +4,14 @@ function Grooming(props : any) {
     return (
         <div>
             <h1 className="title"> DIY Grooming Tips:</h1>
-            <p>Ever been to 5 different groomers asking for a puppy cut and having your dog come out looking different every time? Or do you have a disaster story from the dog groomer who you still had to pay over a hundred dollars?</p>
-            <p>You only have two options: Continue searching for a groomer you like or take matters in your hands and do it yourself. My best advice is to have an idea of what you would like for your dog. Go to the groomer armed with pictures if you're not sure and they can also tell you if the style will work with your dog's type of hair/fur. Be clear about what you want or don't want. Also, if you don't want your dog to be shaved, make sure to specify a length for his/her hair.</p>
-    <p>If you would like to do it yourself, that's great! Your dog will be comfortable around you, so you have an advantage already. The first thing you need to do is establishing trust for grooming. Your dog needs to know that whatever you do is not going to hurt and is not scary. Only when your dog has full trust in you, will they relax, fall asleep and close their eyes during the grooming session.
+            <p>Ever been to 5 different dog groomers in NYC asking for a puppy cut and having your dog come out looking different every time? Or do you have a disaster story from the groomers who you would prefer not to return back to?
 </p>
-<p>You must be relaxed and confident. Dogs are so smart, they will sense right away if you are tense or nervous and this will make them more nervous as well.
-I use treats for positive reinforcement and introduce things slowly. If you're having trouble and your dog is scared, take a look at the tips at the end of the article.
- </p>
- <p>I prefer to use wireless, rechargeable trimmers on the torso for a very even look. It's fast, covering a large surface area and with the comb attachments for varying length, you have a lot to play with. There are typical types of cuts used for different breeds so refer to those guidelines if you don't know where to start. In general, you must always use the trimmers in the same direction of the hair/fur.</p>
-        </div>
+<p></p>
+<p>You only have two options:</p>
+<p>1. Continue searching for a groomer you like</p>
+<p>2. Take matters in your hands and do it yourself (I choose to groom Chiffon myself). Here are some of my tips below:</p>
+<p></p>
+            </div>
     )
 }
 
