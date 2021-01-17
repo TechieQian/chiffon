d697728e11823835bfaeeb7f5687c263a2d34822
Qian Wan <qiansan@gmail.com>
2021-01-17 15:56:42 -0500

    editing grooming

--- src/body/Grooming.tsx
+++ src/body/Grooming.tsx
@@ -9,10 +9,10 @@ function Grooming(props : any) {
 <p>You only have two options:</p>
 <p className="pl-6">1. Continue searching for a groomer you like</p>
 <p className="pl-6">2. Take matters in your hands and do it yourself (I choose to groom Chiffon myself). Here are some of my tips below:</p>
-<p className="is-italic and has-text-weight-semibold" >Establish Trust</p>
+<p className="is-italic and has-text-weight-semibold">Establish Trust</p>
 <p>The first thing you need to do is establish trust when it comes to dog grooming. Your dog is going to be more comfortable around you versus its groomer, so you have an advantage already. Your dog needs to know that whatever you do, it is not going to hurt and it will not be scary. Once Chiffon realized I wasn’t going to ruin her perfect hair (or hurt her), she began to relax and will now even fall asleep during the grooming session.</p>
 <p>Your body language is also super important. Dogs are so smart, they will sense right away if you are tense or nervous and this will make them more nervous as well. I use treats for positive reinforcement and introduce things slowly. If you're having trouble and your dog is scared, take a look at the tips at the end of the article.</p>
-<p className="is-italic and has-text-weight-semibold" >>Getting Ready for Trimming</p>
+<p className="is-italic and has-text-weight-semibold">Getting Ready for Trimming</p>
 <p>Before trimming, you must brush your dog's fur thoroughly. Next, give your dog a full bath, towel them dry and then blow dry. While blow drying, begin styling by giving extra lift to any areas where you want more floof by blowing against the hair/fur direction and combing up and blowing the roots. To open up the eyes, make sure to blow from the front of the face to keep the hair away from the eyes.</p>
 <p className="is-italic and has-text-weight-semibold">Trimming My Dog’s Hair</p>
 <p>I prefer to use wireless, rechargeable trimmers on the torso for a very even look. It's fast, covering a large surface area, and with the comb attachments for varying length, you have a lot to play with. There are typical types of cuts used for different breeds so refer to those guidelines if you don't know where to start. In general, you must always use the trimmers in the same direction of the hair/fur.</p>
