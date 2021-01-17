0b89d947b0a841428bb0567b4ebc72e5083d0bb7
Qian Wan <qiansan@gmail.com>
2021-01-17 15:55:54 -0500

    editing grooming

--- src/body/Grooming.tsx
+++ src/body/Grooming.tsx
@@ -12,12 +12,18 @@ function Grooming(props : any) {
 <p className="is-italic and has-text-weight-semibold" >Establish Trust</p>
 <p>The first thing you need to do is establish trust when it comes to dog grooming. Your dog is going to be more comfortable around you versus its groomer, so you have an advantage already. Your dog needs to know that whatever you do, it is not going to hurt and it will not be scary. Once Chiffon realized I wasn’t going to ruin her perfect hair (or hurt her), she began to relax and will now even fall asleep during the grooming session.</p>
 <p>Your body language is also super important. Dogs are so smart, they will sense right away if you are tense or nervous and this will make them more nervous as well. I use treats for positive reinforcement and introduce things slowly. If you're having trouble and your dog is scared, take a look at the tips at the end of the article.</p>
-<p></p>
+<p className="is-italic and has-text-weight-semibold" >>Getting Ready for Trimming</p>
 <p>Before trimming, you must brush your dog's fur thoroughly. Next, give your dog a full bath, towel them dry and then blow dry. While blow drying, begin styling by giving extra lift to any areas where you want more floof by blowing against the hair/fur direction and combing up and blowing the roots. To open up the eyes, make sure to blow from the front of the face to keep the hair away from the eyes.</p>
-
+<p className="is-italic and has-text-weight-semibold">Trimming My Dog’s Hair</p>
 <p>I prefer to use wireless, rechargeable trimmers on the torso for a very even look. It's fast, covering a large surface area, and with the comb attachments for varying length, you have a lot to play with. There are typical types of cuts used for different breeds so refer to those guidelines if you don't know where to start. In general, you must always use the trimmers in the same direction of the hair/fur.</p>
-
-   
+<p>Choose a comb length for the torso and start from the neck down the back (I use 1" to keep her extra fluffy or 3/4" for her current cut). Lift up the front paws and trim the belly from the chest back (3/4" longer hair, 1/2" shorter). You can also lift up one paw at a time. Lift up the chin and trim from the chin down. Because Chiffon is so small, I just use scissors but you can choose a comb length for the legs and trim down towards the paws. Remove the comb to trim the pads of the feet. Next, use the scissors to trim the face, tail and ears to the desired look. You can also use the scissors or the electric trimmers if you are comfortable to trim the hair around the privates.</p>
+<p className="is-italic and has-text-weight-semibold">Help! My Dog is Scared of...</p>
+<p>Dog Electric Clippers: If your dog has never had electric clippers before, turn it on away from him/her and make sure they are comfortable with the sound. Next, spend a moment to comb through the hair/fur with the clippers turned OFF so they get used to the feeling. If they are comfortable with that, take a few minutes to turn the clippers on and put the handle on their torso to get them used to the vibration. Now, you should be able to use the clippers normally.</p>   
+<p>Dog Blow Dryers: Turn it on away from your dog to make sure they become accustomed to the noise. Blow from a whole arm’s length away at their torso (NOT their face yet) to get them thinking it's just like a little wind. Once they are used to it, you can blow a little on their face from an arm’s length away. As they become comfortable, bring the blow dryer closer, little by little, being careful of the heat and keep changing the spot you are blowing on until you are able to use the blow dryer without any issues.</p>
+<p>Dog Scissors: Some dogs are scared of things coming close to their face (that’s what she said ha). Take it slowly and make sure to hold the pair of scissors horizontally, flat to the face, to avoid any injury. Pretend it’s like a comb, and comb back and forth with the scissors on the muzzle until your dog feels safe enough to close their eyes. Make a snip in the air once in awhile so they get used the sound of the scissors. Repeat on any area where they get scared and go slowly until they get used to it.
+</p>
+<p>It takes patience and practice, but we all know looking beautiful takes hard work. 
+</p>
             </div>
     )
 }
